31500bc740d38b15c6fcc31ec561dc01e4dc481d
Archimedes Trajano <developer@trajano.net>
2018-04-04 00:25:35 -0400

    uncomment

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 docker build -t rootfsimage .
 id=$(docker create rootfsimage -h) # id was cd851ce43a403 when the image was created
 mkdir -p build/rootfs
-exit 0
 docker export "$id" | tar -x -C build/rootfs
 docker rm -vf "$id"
 docker rmi rootfsimage

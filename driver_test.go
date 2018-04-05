96daf93fc2f34373e923f7b12ea996e0b853fb99
Archimedes Trajano <developer@trajano.net>
2018-04-05 01:37:00 -0400

    X

--- driver_test.go
+++ driver_test.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"testing"
+
+	"github.com/docker/go-plugins-helpers/volume"
 )
+
+type testDriver struct {
+	MountedVolumeDriver
+}
+
+func (p *testDriver) Validate(req volume.CreateRequest) error {
+
+	return nil
+}
+
+func (p *testDriver) MountOptions(req volume.CreateRequest) []string {
+
+	var args []string
+	return args
+}
 
 func TestCreate(t *testing.T) {
 	d := *NewMountedVolumeDriver("glusterfs", true, "gfs")
+	d.Create(&volume.CreateRequest{
+		Name: "test",
+	})
 }

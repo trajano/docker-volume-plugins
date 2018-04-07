8a3b2a00744bceed50d54c6fe963ef91e02faeb5
Archimedes Trajano <developer@trajano.net>
2018-04-07 11:39:54 -0400

    initial cifs

---
+++ cifs/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"../mounted-volume"
+	"github.com/docker/go-plugins-helpers/volume"
+)
+
+type gfsDriver struct {
+	servers []string
+	mountedvolume.MountedVolumeDriver
+}
+
+func (p *gfsDriver) Validate(req *volume.CreateRequest) error {
+
+	log.Println("Validate", req)
+	_, serversDefinedInOpts := req.Options["servers"]
+	_, glusteroptsInOpts := req.Options["glusteropts"]
+
+	if len(p.servers) > 0 && (serversDefinedInOpts || glusteroptsInOpts) {
+		return fmt.Errorf("SERVERS is set, options are not allowed")
+	}
+	if serversDefinedInOpts && glusteroptsInOpts {
+		return fmt.Errorf("servers is set, glusteropts are not allowed")
+	}
+	if len(p.servers) == 0 && !serversDefinedInOpts && !glusteroptsInOpts {
+		return fmt.Errorf("One of SERVERS, driver_opts.servers or driver_opts.glusteropts must be specified")
+	}
+
+	return nil
+}
+
+func (p *gfsDriver) MountOptions(req *volume.CreateRequest) []string {
+
+	log.Println("MountOptions", req)
+	servers, serversDefinedInOpts := req.Options["servers"]
+	glusteropts, _ := req.Options["glusteropts"]
+
+	var args []string
+
+	if len(p.servers) > 0 {
+		for _, server := range p.servers {
+			args = append(args, "-s", server)
+		}
+		args = AppendVolumeOptionsByVolumeName(args, req.Name)
+	} else if serversDefinedInOpts {
+		for _, server := range strings.Split(servers, ",") {
+			args = append(args, "-s", server)
+		}
+		args = AppendVolumeOptionsByVolumeName(args, req.Name)
+	} else {
+		args = strings.Split(glusteropts, " ")
+	}
+
+	return args
+}
+
+// AppendVolumeOptionsByVolumeName appends the command line arguments into the current argument list given the volume name
+func AppendVolumeOptionsByVolumeName(args []string, volumeName string) []string {
+	parts := strings.SplitN(volumeName, "/", 2)
+	ret := append(args, "--volfile-id="+parts[0])
+	if len(parts) == 2 {
+		ret = append(ret, "--subdir-mount=/"+parts[1])
+	}
+	return ret
+}
+
+func buildGfsDriver() *gfsDriver {
+	var servers []string
+	if os.Getenv("SERVERS") != "" {
+		servers = strings.Split(os.Getenv("SERVERS"), ",")
+	}
+	d := &gfsDriver{
+		MountedVolumeDriver: *mountedvolume.NewMountedVolumeDriver("glusterfs", true, "gfs"),
+		servers:             servers,
+	}
+	d.Init(d)
+	return d
+}
+
+func main() {
+	d := buildGfsDriver()
+	d.ServeUnix()
+}

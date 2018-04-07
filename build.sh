bd68c6d12db65379747d6e0cf19b4bd6ea9b2fbb
Archimedes Trajano <developer@trajano.net>
2018-04-07 15:54:55 -0400

    refactored

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/sh -e
-docker build -t rootfsimage glsuterfs-volume-plugin
-id=$(docker create rootfsimage -h) # id was cd851ce43a403 when the image was created
-rm -rf build/rootfs
-mkdir -p build/rootfs
-docker export "$id" | tar -x -C build/rootfs
-docker rm -vf "$id"
-docker rmi rootfsimage
-cp config.json build
-docker plugin create trajano/glusterfs-volume-plugin build
+build() {
+    docker build -t rootfsimage $1
+    id=$(docker create rootfsimage -h) # id was cd851ce43a403 when the image was created
+    rm -rf build/rootfs
+    mkdir -p build/rootfs
+    docker export "$id" | tar -x -C build/rootfs
+    docker rm -vf "$id"
+    docker rmi rootfsimage
+    cp $1/config.json build
+    docker plugin create trajano/$1 build
+}
+build glusterfs-volume-plugin
+build cifs-volume-plugin

6315d9e05cbdd450229e129a37cbd981d51034f8
Archimedes Trajano <developer@trajano.net>
2018-04-07 23:10:23 -0400

    add tests

---
+++ cifs-volume-plugin/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/trajano/docker-volume-plugins/mounted-volume"
+)
+
+func TestCalculateCredentialsFile(t *testing.T) {
+	d := &cifsDriver{
+		MountedVolumeDriver: *mountedvolume.NewMountedVolumeDriver("glusterfs", true, "gfs"),
+		credentialPath:      "/foo/bar",
+	}
+	if d.calculateCredentialsFile(filepath.SplitList("foopath/foo/bar/path")) != "" {
+		fmt.Errorf("did not expect file to exist")
+	}
+}

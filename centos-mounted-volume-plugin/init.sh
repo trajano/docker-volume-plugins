14713d6b43712443fdc8697e61c696fba76f1473
Archimedes Trajano <developer@trajano.net>
2018-04-16 16:08:29 -0400

    Removed /etc/mtab

--- centos-mounted-volume-plugin/init.sh
+++ centos-mounted-volume-plugin/init.sh
@@ -4,6 +4,7 @@ echo MOUNT_OPTIONS=${MOUNT_OPTIONS} >> /pluginenv
 echo MOUNT_TYPE=${MOUNT_TYPE} >> /pluginenv
 echo http_proxy=${http_proxy} >> /pluginenv
 cat /pluginenv
-mount -t cgroup cgroup /sys/fs/cgroup
+ls -lR /sys/fs/cgroup
+#mount -t cgroup cgroup /sys/fs/cgroup
 /ps.sh &
 exec /sbin/init

6e118c05c66effb3f5386192a23fa68339d7d469
Archimedes Trajano <developer@trajano.net>
2018-04-25 12:22:51 -0400

    Added the use of boltdb to store the data

--- mounted-volume/driver_test.go
+++ mounted-volume/driver_test.go
@@ -1,8 +1,10 @@
 package mountedvolume
 
 import (
+	"os"
 	"testing"
 
+	"github.com/boltdb/bolt"
 	"github.com/docker/go-plugins-helpers/volume"
 )
 
@@ -39,4 +41,42 @@ func TestCreate(t *testing.T) {
 	d.Create(&volume.CreateRequest{
 		Name: "test",
 	})
+}
+
+func TestDatabase(t *testing.T) {
+
+	d := &testDriver{
+		Driver: *NewDriver("glusterfs", true, "gfs", "global"),
+	}
+	defer d.Close()
+	defer os.Remove("volumes.db")
+
+	status := make(map[string]interface{})
+	status["mounted"] = false
+	status["args"] = "args"
+
+	if err := d.volumedb.Update(func(tx *bolt.Tx) error {
+		d.storeVolumeInfo(tx, "test", &mountedVolumeInfo{
+			Options:    make(map[string]string),
+			MountPoint: "hello",
+			Args:       []string{"test", "foo"},
+			Status:     status,
+		})
+		return tx.Commit()
+	}); err != nil {
+		t.Fail()
+	}
+
+	if err := d.volumedb.Update(func(tx *bolt.Tx) error {
+		d.storeVolumeInfo(tx, "test", &mountedVolumeInfo{
+			Options:    make(map[string]string),
+			MountPoint: "hello-again",
+			Args:       []string{"test", "foo"},
+			Status:     status,
+		})
+		return tx.Commit()
+	}); err != nil {
+		t.Fail()
+	}
+
 }
